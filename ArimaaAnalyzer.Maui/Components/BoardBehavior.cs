e2c878795cd33d7cf29bde4126336b6fa6a37f21
cmlykke <cmlykke@hotmail.com>
2025-11-24 19:02:25 +0100

    added parameter class to control board behavior

---
+++ ArimaaAnalyzer.Maui/Components/BoardBehavior.cs
@@ -0,0 +1,25 @@
+﻿namespace ArimaaAnalyzer.Maui.Components;
+
+public class BoardBehavior
+{
+    public bool IsInteractive { get; init; }
+    
+
+    /// <summary>
+    /// Standard board for human play.
+    /// </summary>
+    public static BoardBehavior Playable => new() 
+    { 
+        IsInteractive = true, 
+       
+    };
+
+    /// <summary>
+    /// Read-only board for displaying AI moves or history.
+    /// </summary>
+    public static BoardBehavior Spectator => new() 
+    { 
+        IsInteractive = false, 
+        
+    };
+}
