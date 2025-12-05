b48cdd240379632a9452c878e8d8c7748ed84596
Weiqifan1 <cmlykke@hotmail.com>
2025-12-05 19:08:10 +0100

    added settings class for the board

---
+++ ArimaaAnalyzer.Maui/Components/ArimaaBoardSettings.cs
@@ -0,0 +1,63 @@
+﻿using ArimaaAnalyzer.Maui.Services.Arimaa;
+
+namespace ArimaaAnalyzer.Maui.Components;
+
+public sealed class ArimaaBoardSettings
+{
+    public double SquareSizePx { get; init; } = 56; // maps to --square-size
+    public double PieceSizePx { get; init; } = 44;  // maps to --piece-size
+    public bool ShowOuterUi { get; init; } = false;
+    public BoardBehavior Behavior { get; init; } = BoardBehavior.Playable;
+
+    // Presets
+    public static ArimaaBoardSettings PlayableSmall => new()
+    {
+        SquareSizePx = 44,
+        PieceSizePx = 36,
+        ShowOuterUi = false,
+        Behavior = BoardBehavior.Playable
+    };
+
+    public static ArimaaBoardSettings PlayableMedium => new()
+    {
+        SquareSizePx = 56,
+        PieceSizePx = 44,
+        ShowOuterUi = false,
+        Behavior = BoardBehavior.Playable
+    };
+
+    public static ArimaaBoardSettings PlayableLarge => new()
+    {
+        SquareSizePx = 72,
+        PieceSizePx = 58,
+        ShowOuterUi = true,
+        Behavior = BoardBehavior.Playable
+    };
+
+    public static ArimaaBoardSettings SpectatorSmall => new()
+    {
+        SquareSizePx = 36,
+        PieceSizePx = 30,
+        ShowOuterUi = false,
+        Behavior = BoardBehavior.Spectator
+    };
+
+    public static ArimaaBoardSettings SpectatorMedium => new()
+    {
+        SquareSizePx = 48,
+        PieceSizePx = 40,
+        ShowOuterUi = false,
+        Behavior = BoardBehavior.Spectator
+    };
+
+    public static ArimaaBoardSettings SpectatorLarge => new()
+    {
+        SquareSizePx = 64,
+        PieceSizePx = 52,
+        ShowOuterUi = true,
+        Behavior = BoardBehavior.Spectator
+    };
+
+    // Default
+    public static ArimaaBoardSettings Default => PlayableMedium;
+}
