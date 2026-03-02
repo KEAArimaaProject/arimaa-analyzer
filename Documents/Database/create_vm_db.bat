8eec8a1fff2d8d2e02d4c28b9664ef826927f269
MaxusTheOne <markus@bille.dk>
2026-03-02 08:43:33 +0100

    Changed script targeting the sql, so that it works again x)

--- Documents/Database/create_vm_db.bat
+++ Documents/Database/create_vm_db.bat
@@ -5,7 +5,7 @@ REM Create db in a virtual environment. Cd into /database before running in comm
 
 docker-compose up -d
 timeout /t 10
-mysql --host=localhost --port=3307 --user=root --password=123456 < Sql_arimaa_init.sql
-echo Database setup completed successfully!
-@REM docker compose down to remove environment
+mysql --host=localhost --port=3307 --user=root --password=123456 < .\mysql\Sql_arimaa_init.sql
+echo Database setup completed successfully.. I hope!
+@REM "docker compose down" to remove environment
 pause
